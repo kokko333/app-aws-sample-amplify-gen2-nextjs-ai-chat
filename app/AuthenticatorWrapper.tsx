c2e55743c634190582ebf2cb828ceb9ead6c1302
kokko333 <k.asako.tenor1@gmail.com>
2026-01-22 13:15:31 +0900

    css整理

--- app/AuthenticatorWrapper.tsx
+++ app/AuthenticatorWrapper.tsx
@@ -1,6 +1,11 @@
 "use client";
 
 import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(outputs);
 
 export default function AuthenticatorWrapper({
   children,
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./app.css";
 
 import AuthenticatorWrapper from "./AuthenticatorWrapper";
-import "@aws-amplify/ui-react/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
