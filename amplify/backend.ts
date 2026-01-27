1bad96a740777b6c517e9123d092fe65f7a5171e
kokko333 <k.asako.tenor1@gmail.com>
2026-01-27 12:04:57 +0900

    バックエンドリソースへtag付与

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,15 @@
 import { defineBackend } from "@aws-amplify/backend";
+import { Tags } from "aws-cdk-lib";
 import { auth } from "./auth/resource.js";
 import { chatData, todoData } from "./data/resource.js";
 
-defineBackend({
+const backend = defineBackend({
   auth,
   chatData,
   todoData,
 });
+
+const tags = Tags.of(backend.stack);
+tags.add("Billing", "amplify-nextjs-sample");
+tags.add("Project", "amplify-nextjs-sample");
+tags.add("Environment", "development");
